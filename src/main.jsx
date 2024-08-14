1c729f5023a11121d774fa1d21a55964ffa65f03
Yurgish <humenyuriipetrovych@gmail.com>
2024-08-14 18:15:42 +0300

    updated to browser router to hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 import "overlayscrollbars/overlayscrollbars.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/Life-Timer">
+        <HashRouter basename="/Life-Timer">
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
